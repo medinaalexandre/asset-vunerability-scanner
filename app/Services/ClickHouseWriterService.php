6b2f3a2e8fa6193b8c7380911f5f1d69dcf6da0a
Medina <alexandre.medina@logcomex.com>
2025-11-05 09:19:35 -0300

    feat: persist new vulnerability attach on clickhouse

--- app/Services/ClickHouseWriterService.php
+++ app/Services/ClickHouseWriterService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Asset;
 use App\Models\Vulnerability;
 use App\Models\VulnerabilityFact;
 use Carbon\Carbon;
@@ -14,18 +15,28 @@ class ClickHouseWriterService
         $facts = [];
 
         foreach ($vulnerability->assets as $asset) {
-            $facts[] = [
-                'asset_id' => $asset->id,
-                'cve_id' => $vulnerability->cve_id,
-                'asset_criticality' => $asset->criticality_level->value,
-                'asset_weight' => $asset->criticality_level->getWeight(),
-                'cvss_base_score' => $vulnerability->cvss_base_score,
-                'calculated_severity' => $vulnerability->severity->value,
-                'published_at' => $vulnerability->published_at,
-                'updated_at' => Carbon::now()->toDateTimeString(),
-            ];
+            $facts[] = $this->prepareData($asset, $vulnerability);
         }
 
         VulnerabilityFact::prepareAndInsertBulk($facts);
+    }
+
+    public function upsertVulnerabilityAssetFact(Asset $asset, Vulnerability $vulnerability): void
+    {
+        VulnerabilityFact::create($this->prepareData($asset, $vulnerability));
+    }
+
+    private function prepareData(Asset $asset, Vulnerability $vulnerability): array
+    {
+        return [
+            'asset_id' => $asset->id,
+            'cve_id' => $vulnerability->cve_id,
+            'asset_criticality' => $asset->criticality_level->value,
+            'asset_weight' => $asset->criticality_level->getWeight(),
+            'cvss_base_score' => $vulnerability->cvss_base_score,
+            'calculated_severity' => $vulnerability->severity->value,
+            'published_at' => $vulnerability->published_at,
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ];
     }
 }
